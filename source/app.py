8e60f14627995171282e9fa7aea7b2b18280c70b
Mike Hennelly <mike@mhennelly.com>
2022-09-17 23:01:54 -0700

    Defined Models

--- source/app.py
+++ source/app.py
@@ -1,9 +1,12 @@
 import config
 from flask import Flask
+from models import Item, Order, db
 
 app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig)
+db.init_app(app)
 
 
+@app.route("/")
 def index():
-    return
+    return Item.query.all(), Order.query.all()
